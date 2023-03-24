4c90508f8495e78de6fcd63a43ff945687886e33
Sushant <sushantnambiar@gmail.com>
2023-03-24 20:07:51 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,68 @@
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+
 function App() {
-  return <div className="center">HEllo WOrld</div>;
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=a4b7836751397f65718a2096cd672c2a`;
+
+  const searchLocation = e => {
+    if (e.key === "Enter") {
+      axios.get(url).then(response => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+    }
+  };
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={e => setLocation(e.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{data.main.feels_like}</p>
+              ) : null}
+              <p>Feels like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.main ? <p className="bold">{data.wind.speed}MPH</p> : null}
+              <p>Wind speed</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
